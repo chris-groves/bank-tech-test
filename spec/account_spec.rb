57256e7083336126e5ec35a696fe27079aaaa8c0
Chris Groves <chris_groves87@hotmail.co.uk>
2019-07-29 12:05:49 +0100

    test: user can make a deposit

--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -5,4 +5,10 @@ describe Account do
     account = Account.new
     expect(account.balance).to eq 0
   end
+
+  it 'can receive a deposit of money' do
+    account = Account.new
+    account.deposit(1000)
+    expect(account.balance).to eq 1000
+  end
 end
