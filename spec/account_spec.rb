e1807759fe004018716129a7392d5ce6b4a01b0b
Chris Groves <chris_groves87@hotmail.co.uk>
2019-07-31 10:27:50 +0100

    refactor: refactor test layout

--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -2,49 +2,51 @@ require 'account'
 require 'date'
 
 describe Account do
-  it 'initializes with a balance of 0' do
-    account = Account.new
-    expect(account.balance).to eq 0
+  describe '#balance' do
+    it 'initializes with a balance of 0' do
+      account = Account.new
+      expect(account.balance).to eq 0
+    end
+
+
+    it 'can receive a deposit of money' do
+      account = Account.new
+      account.deposit(1000)
+      expect(account.balance).to eq 1000
+    end
+
+    it 'can receive a withdrawal of money' do
+      account = Account.new
+      account.deposit(1000)
+      account.withdraw(500)
+      expect(account.balance).to eq 500
+    end
   end
 
-  it 'can receive a deposit of money' do
-    account = Account.new
-    account.deposit(1000)
-    expect(account.balance).to eq 1000
+  describe '#transaction_history' do
+    it 'initializes with an empty transaction history' do
+      account = Account.new
+      expect(account.transaction_history).to eq []
+    end
+
+    it 'stores the details of a deposit' do
+      account = Account.new
+      account.deposit(1000)
+      expect(account.transaction_history).to eq ["#{Account::CURRENT_DATE} || 1000 || || 1000"]
+    end
+
+    it 'stores the details of a withdrawal' do
+      account = Account.new
+      account.withdraw(500)
+      expect(account.transaction_history).to eq ["#{Account::CURRENT_DATE} || || 500 || -500"]
+    end
   end
 
-  it 'can receive a withdrawal of money' do
-    account = Account.new
-    account.deposit(1000)
-    account.withdraw(500)
-    expect(account.balance).to eq 500
-  end
-
-  it 'initializes with an empty transaction history' do
-    account = Account.new
-    expect(account.transaction_history).to eq []
-  end
-
-  it 'stores the details of a deposit' do
-    account = Account.new
-    account.deposit(1000)
-    expect(account.transaction_history).to eq ["#{Account::CURRENT_DATE} || 1000 || || 1000"]
-  end
-
-  it 'stores the details of a withdrawal' do
-    account = Account.new
-    account.withdraw(500)
-    expect(account.transaction_history).to eq ["#{Account::CURRENT_DATE} || || 500 || -500"]
-  end
-
-  it 'prints out statement headings' do
-    account = Account.new
-    expect(Account::STATEMENT_HEADINGS).to eq "date || credit || debit || balance"
-  end
-
-  it 'prints out statement with headings & a transaction' do
-    account = Account.new
-    account.deposit(1000)
-    expect(account.print_statement).to eq "date || credit || debit || balance" + "\n" + "#{Account::CURRENT_DATE} || 1000 || || 1000"
+  describe '#print_statement' do
+    it 'prints out statement with headings & a transaction' do
+      account = Account.new
+      account.deposit(1000)
+      expect(account.print_statement).to eq "date || credit || debit || balance" + "\n" + "#{Account::CURRENT_DATE} || 1000 || || 1000"
+    end
   end
 end
